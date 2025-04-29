d3f25d7eba470ed4c031a4f809deed16d9f72be9
inornnut01 <inornnut01@gmail.com>
2025-04-29 20:23:21 +0700

    start set up 1 mongoose and mongodb

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/products", (req, res) => {
+  
+});
+
+
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Sever stared at http://localhost:5000");
+});
+
+
+
