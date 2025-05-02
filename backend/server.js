824c9902d7fdb6476d5c3b17149f53403eb953cd
inornnut01 <inornnut01@gmail.com>
2025-05-02 18:01:26 +0700

    finish project

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -8,9 +9,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accpect JSON data in the req.body
 
 app.use("/api/products", productRoutes)
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/vite-project/dist")));
+  app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend/vite-project", "dist", "index.html"));
+	});
+}
 
 // Postman
 
